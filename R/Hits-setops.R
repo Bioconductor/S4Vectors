96920043752685b150d08866629ff3ac372affaf
Herve Pages <hpages@fhcrc.org>
2016-05-20 01:24:55 +0000

    add "union", "intersect", "setdiff", and "setequal" methods for Vector objects
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/S4Vectors@117697 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Hits-setops.R
+++ R/Hits-setops.R
@@ -2,19 +2,13 @@
 ### Set operations
 ### -------------------------------------------------------------------------
 ###
-### union(), intersect(), and setdiff() are endomorphisms with respect to
-### their first argument 'x'.
+### The methods below are endomorphisms with respect to their first argument
+### 'x'. They propagates the names and metadata columns.
 ###
+
 
 setMethod("union", c("Hits", "Hits"),
     function(x, y)
-    {
-        ans <- unique(c(as(x, "Hits"), as(y, "Hits")))
-        as(ans, class(x))
-    }
+        as(callNextMethod(as(x, "Hits"), as(y, "Hits")), class(x))
 )
-
-setMethod("intersect", c("Hits", "Hits"), function(x, y) unique(x[x %in% y]))
-
-setMethod("setdiff", c("Hits", "Hits"), function(x, y) unique(x[!(x %in% y)]))
 
