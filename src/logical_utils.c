b5ef756e522dcc66b46fc90a526e42b8ec2e729c
Herve Pages <hpages@fhcrc.org>
2017-06-08 02:50:10 +0000

    more timings of base::sum() vs logical_sum() vs logical2_sum()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/S4Vectors@130172 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/logical_utils.c
+++ src/logical_utils.c
@@ -3,10 +3,37 @@
 // R_XLEN_T_MAX is 2^52
 // LLONG_MAX    is 2^63-1
 
-/* Unlike base::sum() which can overflow (and return NA_integer_) on a long
-   logical vector, logical_sum() never overflows. It returns a double if the
-   result cannot be represented as an int. Note that this is what length()
-   does. */
+/*
+  Unlike base::sum() which can overflow (and return NA_integer_) on a long
+  logical vector, logical_sum() never overflows. It returns a double if the
+  result cannot be represented as an int. Note that this is what length() does.
+  Note that logical_sum() is slightly faster than base::sum():
+
+            length(x)  base::sum()  logical_sum()  speedup
+            ---------  -----------  -------------  -------
+    rhino3:
+               1e8         83 ms          74 ms       12%
+               1e9        0.84 s         0.75 s       12%
+               3e9        2.52 s         2.35 s       13%  <-- long vector
+    malbec1:
+               1e8         93 ms          74 ms       26%
+               1e9        0.92 s         0.75 s       23%
+    veracruz1:
+               1e8        121 ms          93 ms       30%
+               1e9        1.27 s         1.01 s       26%
+
+  - rhino3: Linux server, Intel(R) Xeon(R) CPU E5-2697 v3 @ 2.60GHz (56
+    cores), 384 GB of RAM, with Ubuntu Ubuntu 14.04.3 LTS, gcc 4.8.4,
+    R 3.4.0 installed from source (default compiler options and flags).
+  - malbec1: HP ProLiant DL360 Gen9 server, Intel(R) Xeon(R) CPU E5-2640
+    v4 @ 2.40GHz (20 cores), 32 GB of RAM, with Ubuntu 16.04.2 LTS, gcc 5.4.0,
+    R 3.4.0 installed from source (default compiler options and flags).
+  - veracruz1: virtualized Mac Pro Server at Mac Stadium
+    (https://www.macstadium.com), Quad-Core Intel Xeon E5 3.7 GHz,
+    32 GB of RAM, with El Capitan, clang 4.0.0, R 3.4.0 (CRAN binary).
+
+  I did not time this on Windows.
+*/
 SEXP logical_sum(SEXP x, SEXP na_rm)
 {
 	R_xlen_t x_len, sum, i;
@@ -39,42 +66,53 @@ SEXP logical_sum(SEXP x, SEXP na_rm)
 }
 
 /*
-   Storing logical vectors in int arrays like R does is such a waste of
-   memory!
+  Playing around with logical vectors stored in char arrays.
 
-   Playing around with logical vectors stored in char arrays:
+  Storing logical vectors in int arrays like R does is such a waste of memory!
+  By using chars instead of ints very common operations like sum(x < 0.9) (this
+  is probably the primary use case for sum()!) would require 4x less memory.
+  This is particularly relevant if 'x' is a long vector (e.g. length(x) = 3e9)
+  where R currently spends a significant amount of time allocating memory (e.g.
+  12Gb) to store the temporary logical vector.
 
-   - Very common operations like sum(x < 0.9) (this is probably the primary
-     use case for sum()!) would require 4x less memory. This is particularly
-     relevant if 'x' is a long vector (e.g. length(x) = 3e9) where R currently
-     spends a significant amount of time allocating memory (e.g. 12Gb) to store
-     the temporary logical vector.
+  Note that walking on a char array is significantly faster than base::sum() on
+  Linux but not on Mac where it's more than 3x slower:
 
-   - As a bonus logical2_sum() below is slightly faster (about 7%) than
-     base::sum() on my laptop (Ubuntu Dell LATITUDE E6440 with 4Gb of RAM
-     and running 64-bit Ubuntu 14.04.5 LTS). This might be platform specific
-     though...
+            length(x)  base::sum()  logical2_sum()  speedup
+            ---------  -----------  --------------  -------
+    rhino3:
+               1e8         83 ms           66 ms       25%
+               1e9        0.84 s          0.66 s       27%
+               3e9        2.52 s          1.93 s       30%  <-- long vector
+    malbec1:
+               1e8         93 ms           64 ms       45%
+               1e9        0.92 s          0.63 s       46%
+    veracruz1:
+               1e8        121 ms          398 ms       disaster!
+               1e9        1.27 s          4.05 s       disaster!
 
-   To compare base::sum() vs logical_sum() vs logical2_sum():
+  To compare base::sum() vs logical_sum() vs logical2_sum():
 
-     library(S4Vectors)
-     sum1 <- function(x, na.rm=FALSE) .Call("logical_sum", x, na.rm, PACKAGE="S4Vectors")
-     sum2 <- function(x, na.rm=FALSE) .Call("logical2_sum", x, na.rm, PACKAGE="S4Vectors")
+    library(S4Vectors)
+    sum1 <- function(x, na.rm=FALSE)
+            .Call("logical_sum", x, na.rm, PACKAGE="S4Vectors")
+    sum2 <- function(x, na.rm=FALSE)
+            .Call("logical2_sum", x, na.rm, PACKAGE="S4Vectors")
 
-     x <- as.logical(sample(2L, 1e8, replace=TRUE) - 1L)
-     x2 <- as.raw(x)
+    x <- as.logical(sample(2L, 1e8, replace=TRUE) - 1L)
+    x2 <- as.raw(x)
 
-     ## Correctness
-     res0 <- sum(x, na.rm=FALSE)
-     res1 <- sum1(x, na.rm=FALSE)
-     res2 <- sum2(x2, na.rm=FALSE)
-     stopifnot(identical(res0, res1))
-     stopifnot(identical(res0, res2))
+    ## Correctness
+    res0 <- sum(x, na.rm=FALSE)
+    res1 <- sum1(x, na.rm=FALSE)
+    res2 <- sum2(x2, na.rm=FALSE)
+    stopifnot(identical(res0, res1))
+    stopifnot(identical(res0, res2))
 
-     ## Speed
-     system.time(replicate(20, sum(x, na.rm=FALSE)))
-     system.time(replicate(20, sum1(x, na.rm=FALSE)))
-     system.time(replicate(20, sum2(x2, na.rm=FALSE)))
+    ## Speed
+    system.time(replicate(20, sum(x, na.rm=FALSE)))
+    system.time(replicate(20, sum1(x, na.rm=FALSE)))
+    system.time(replicate(20, sum2(x2, na.rm=FALSE)))
 
 */
 #define	NA_LOGICAL2	127  /*	Arbitrary choice. Could be set to anything
