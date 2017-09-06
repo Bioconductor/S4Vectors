fc161357ff7c01c3b05087cf5ab1553960dc1580
Hervé Pagès <hpages@fredhutch.org>
2017-09-05 22:06:53 -0700

    Temporary workaround to make the "union" method for Hits objects work even in the presence of another "union" generic in the cache (which is the case e.g. if the user loads the lubridate package).

--- R/Hits-setops.R
+++ R/Hits-setops.R
@@ -3,12 +3,16 @@
 ### -------------------------------------------------------------------------
 ###
 ### The methods below are endomorphisms with respect to their first argument
-### 'x'. They propagates the names and metadata columns.
+### 'x'. They propagate the names and metadata columns.
 ###
 
 
 setMethod("union", c("Hits", "Hits"),
     function(x, y)
-        as(callNextMethod(as(x, "Hits"), as(y, "Hits")), class(x))
+        ## callNextMethod() is broken in R <= 3.4 if there is more than
+        ## one "union" generic in the cache (e.g. which is the case if the
+        ## user loads the lubridate package). So we avoid its use for now.
+        #as(callNextMethod(as(x, "Hits"), as(y, "Hits")), class(x))
+        as(union.Vector(as(x, "Hits"), as(y, "Hits")), class(x))
 )
 
