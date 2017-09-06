cd23b9f8895e73ec7f2472edac199ca678998699
Hervé Pagès <hpages@fredhutch.org>
2017-09-05 21:51:06 -0700

    add union, intersect, setdiff, and setequal S3 methods for Vector objects

--- R/Vector-setops.R
+++ R/Vector-setops.R
@@ -6,20 +6,19 @@
 ### 'x'. They propagates the names and metadata columns.
 ###
 
+### S3/S4 combo for union.Vector
+union.Vector <- function(x, y) unique(c(x, y))
+setMethod("union", c("Vector", "Vector"), union.Vector)
 
-setMethod("union", c("Vector", "Vector"),
-    function(x, y) unique(c(x, y))
-)
+### S3/S4 combo for intersect.Vector
+intersect.Vector <- function(x, y) unique(x[x %in% y])
+setMethod("intersect", c("Vector", "Vector"), intersect.Vector)
 
-setMethod("intersect", c("Vector", "Vector"),
-    function(x, y) unique(x[x %in% y])
-)
+### S3/S4 combo for setdiff.Vector
+setdiff.Vector <- function(x, y) unique(x[!(x %in% y)])
+setMethod("setdiff", c("Vector", "Vector"), setdiff.Vector)
 
-setMethod("setdiff", c("Vector", "Vector"),
-    function(x, y) unique(x[!(x %in% y)])
-)
-
-setMethod("setequal", c("Vector", "Vector"),
-    function(x, y) all(x %in% y) && all(y %in% x)
-)
+### S3/S4 combo for setequal.Vector
+setequal.Vector <- function(x, y) all(x %in% y) && all(y %in% x)
+setMethod("setequal", c("Vector", "Vector"), setequal.Vector)
 
