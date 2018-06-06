Bioconductor/S4Vectors

commit 0fcc86844852f7c1379c7a7c7a3ea010dfccc5b5

Add 'nodup' argument to selectHits()

R/Hits-class.R
@@ -423,33 +423,74 @@ setMethod("sort", "SortedByQueryHits",
     callNextMethod()
 })
 
+
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### selectHits()
 ###
 
-selectHits <- function(x,
-                       select=c("all", "first", "last", "arbitrary", "count"),
-                       rank)
+### Return an integer vector parallel to the query (i.e. of length
+### 'nLnode(hits)') except when select="all", in which case it's a no-op.
+###
+### 'nodup' must be TRUE or FALSE (the default) and can only be set to TRUE
+### when 'select' is "first", "last" or "arbitrary", and when the input hits
+### are sorted by query. When 'nodup=TRUE', a given element in the subject is
+### not allowed to be assigned to more than one element in the query, which is
+### achieved by following a simple "first come first served" pairing strategy.
+### So the returned vector is guaranteed to contain unique non-NA values.
+### Note that such vector represents a mapping between the query and subject
+### that is one-to-zero-or-one in *both* directions. So it represents a
+### pairing between the elements in query and subject, where a given element
+### belongs to at most one pair.
+### A note about the "first come first served" pairing strategy: This strategy
+### is simple and fast, but, in general, it won't achieve a "maximal pairing"
+### (i.e. a pairing with the most possible number of pairs) for a given input
+### Hits object. However it actually does produce a maximal pairing if the
+### Hits object is the result of call to findMatches() (with select="all")'.
+### Also, in that case, this pairing strategy is symetric i.e. the resulting
+### pairs are not affected by switching 'x' and 'table' in the call to
+### findMatches() (or by transposing the input Hits object).
+###
+### Finally note that when 'select' is "first" or "last" and 'nodup' is FALSE,
+### or when 'select' is "count", the output of selectHits() is not affected
+### by the order of the hits in the input Hits object.
+selectHits <- function(hits,
+    select=c("all", "first", "last", "arbitrary", "count"),
+    nodup=FALSE,
+    rank)
 {
-    if (!is(x, "Hits"))
-        stop("'x' must be a Hits object")
+    if (!is(hits, "Hits"))
+        stop("'hits' must be a Hits object")
     select <- match.arg(select)
+    if (!isTRUEorFALSE(nodup))
+        stop(wmsg("'nodup' must be TRUE or FALSE"))
+    if (nodup && !(select %in% c("first", "last", "arbitrary")))
+        stop(wmsg("'nodup=TRUE' is only supported when 'select' ",
+                  "is \"first\", \"last\", or \"arbitrary\""))
+    if (!missing(rank) && (!(select %in% c("first", "last")) || nodup))
+        stop(wmsg("'rank' is only supported when 'select' ",
+                  "is \"first\" or \"last\" and 'nodup' is FALSE"))
     if (select == "all")
-        return(x)
-    to <- to(x)
+        return(hits)  # no-op
+
+    hits_from <- from(hits)
+    hits_to <- to(hits)
+    hits_nLnode <- nLnode(hits)
+    hits_nRnode <- nRnode(hits)
+
     if (!missing(rank)) {
-        r <- rank(x, ties.method="first", by=rank)
+        r <- rank(hits, ties.method="first", by=rank)
         revmap <- integer()
-        revmap[r] <- to
-        to <- r
+        revmap[r] <- hits_to
+        hits_to <- r
     }
-    ans <- .Call2("select_hits", from(x), to, nLnode(x), select,
-                  PACKAGE="S4Vectors")
-    if (!missing(rank)) {
+    ans <- .Call2("select_hits", hits_from, hits_to, hits_nLnode, hits_nRnode,
+                                 select, nodup,
+                                 PACKAGE="S4Vectors")
+    if (!missing(rank))
         ans <- revmap[ans]
-    }
     ans
 }
+
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### breakTies()
@@ -467,6 +508,7 @@ breakTies <- function(x, method=c("first", "last"), rank) {
     .new_Hits("SortedByQueryHits", which(!is.na(to)), to[!is.na(to)],
               nLnode(x), nRnode(x), NULL)
 }
+
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### revmap()

man/Hits-class.Rd
@@ -309,8 +309,13 @@ hits <- findMatches(x, table)  # sorts the hits by query
 hits
 
 selectHits(hits, select="all")  # no-op
+
 selectHits(hits, select="first")
+selectHits(hits, select="first", nodup=TRUE)
+
 selectHits(hits, select="last")
+selectHits(hits, select="last", nodup=TRUE)
+
 selectHits(hits, select="arbitrary")
 selectHits(hits, select="count")
 

src/Hits_class.c
@@ -168,16 +168,16 @@ static SEXP new_Hits_with_revmap(const char *classname,
 	return ans;
 }
 
-static int check_nnode(SEXP nnode, const char *side)
+static int get_nnode(SEXP nnode, const char *side)
 {
 	int nnode0;
 
 	if (!IS_INTEGER(nnode) || LENGTH(nnode) != 1)
-		error("'n%snode(x)' must be a single integer",
+		error("'n%snode(hits)' must be a single integer",
                       side);
 	nnode0 = INTEGER(nnode)[0];
 	if (nnode0 == NA_INTEGER || nnode0 < 0)
-		error("'n%snode(x)' must be a single non-negative integer",
+		error("'n%snode(hits)' must be a single non-negative integer",
                       side);
 	return nnode0;
 }
@@ -193,15 +193,15 @@ static int check_hits(const int *from, const int *to, int nhit,
 	for (k = 0; k < nhit; k++, from++, to++) {
 		i = *from;
 		if (i == NA_INTEGER || i < 1 || i > nLnode)
-			error("'from(x)' must contain non-NA values "
-			      ">= 1 and <= 'nLnode(x)'");
+			error("'from(hits)' must contain non-NA values "
+			      ">= 1 and <= 'nLnode(hits)'");
 		if (i < prev_i)
 			already_sorted = 0;
 		prev_i = i;
 		j = *to;
 		if (j == NA_INTEGER || j < 1 || j > nRnode)
-			error("'to(x)' must contain non-NA values "
-			      ">= 1 and <= 'nRnode(x)'");
+			error("'to(hits)' must contain non-NA values "
+			      ">= 1 and <= 'nRnode(hits)'");
 	}
 	return already_sorted;
 }
@@ -217,9 +217,9 @@ SEXP Hits_new(SEXP Class, SEXP from, SEXP to, SEXP nLnode, SEXP nRnode,
 
 	classname = CHAR(STRING_ELT(Class, 0));
 	nhit = _check_integer_pairs(from, to, &from_p, &to_p,
-				    "from(x)", "to(x)");
-	nLnode0 = check_nnode(nLnode, "L");
-	nRnode0 = check_nnode(nRnode, "R");
+				    "from(hits)", "to(hits)");
+	nLnode0 = get_nnode(nLnode, "L");
+	nRnode0 = get_nnode(nRnode, "R");
 	already_sorted = check_hits(from_p, to_p, nhit, nLnode0, nRnode0);
 	if (already_sorted)
 		return new_Hits1(classname, from_p, to_p, nhit,
@@ -273,33 +273,82 @@ int _get_select_mode(SEXP select)
 	return 0;
 }
 
-/* --- .Call ENTRY POINT --- */
-
-SEXP select_hits(SEXP from, SEXP to, SEXP nLnode, SEXP select)
+static int get_nodup(SEXP nodup)
 {
-	int nhit, ans_len, select_mode, init_val, i, k, j1;
+	int nodup0;
+
+	if (!IS_LOGICAL(nodup) || LENGTH(nodup) != 1
+	 || (nodup0 = LOGICAL(nodup)[0]) == NA_LOGICAL)
+		error("'nodup' must be a TRUE or FALSE");
+	return nodup0;
+}
+
+/* --- .Call ENTRY POINT ---
+ * Args:
+ *   from, to, nLnode, nRnode: The 4 slots of a Hits object.
+ *   select: Must be "first" "last", "arbitrary", or "count". Note that 'to'
+ *           is ignored when 'select' is set to "count".
+ *   nodup:  Must be TRUE or FALSE. If TRUE then 'select' must be "first",
+ *           "last" or "arbitrary", and 'from' must be sorted. Note that
+ *           'nRnode' is ignored when 'nodup' is set to FALSE.
+ */
+SEXP select_hits(SEXP from, SEXP to, SEXP nLnode, SEXP nRnode,
+		 SEXP select, SEXP nodup)
+{
+	int nhit, ans_len, select_mode, nodup0,
+	    init_val, i, i_prev, k, *ans_p, ans_elt;
 	const int *from_p, *to_p;
 	SEXP ans;
+	CharAE *is_used;
 
 	nhit = _check_integer_pairs(from, to,
 				    &from_p, &to_p,
-				    "from(x)", "to(x)");
-	ans_len = INTEGER(nLnode)[0];
+				    "from(hits)", "to(hits)");
+	ans_len = get_nnode(nLnode, "L");
 	select_mode = _get_select_mode(select);
+	nodup0 = get_nodup(nodup);
+	if (nodup0) {
+		if (select_mode != FIRST_HIT
+		 && select_mode != LAST_HIT
+		 && select_mode != ARBITRARY_HIT)
+			error("'nodup=TRUE' is only supported when "
+			      "'select' is \"first\", \"last\",\n"
+			      "  or \"arbitrary\"");
+	}
 	PROTECT(ans = NEW_INTEGER(ans_len));
 	init_val = select_mode == COUNT_HITS ? 0 : NA_INTEGER;
-	for (i = 0; i < ans_len; i++)
-		INTEGER(ans)[i] = init_val;
+	for (i = 0, ans_p = INTEGER(ans); i < ans_len; i++, ans_p++)
+		*ans_p = init_val;
+	if (nodup0) {
+		is_used = _new_CharAE(get_nnode(nRnode, "R"));
+		memset(is_used->elts, 0, is_used->_buflength);
+	}
+	i_prev = 0;
 	for (k = 0; k < nhit; k++, from_p++, to_p++) {
 		i = *from_p - 1;
+		ans_p = INTEGER(ans) + i;
 		if (select_mode == COUNT_HITS) {
-			INTEGER(ans)[i]++;
+			(*ans_p)++;
 			continue;
 		}
-		j1 = *to_p;
-		if (INTEGER(ans)[i] == NA_INTEGER
-		 || (select_mode == FIRST_HIT) == (j1 < INTEGER(ans)[i]))
-			INTEGER(ans)[i] = j1;
+		if (nodup0 && k != 0) {
+			if (i < i_prev)
+				error("'nodup=TRUE' is only supported "
+				      "on a Hits object where the hits\n"
+				      "  are sorted by query at the moment");
+			if (i > i_prev) {
+				ans_elt = INTEGER(ans)[i_prev];
+				is_used->elts[ans_elt - 1] = 1;
+			}
+		}
+		i_prev = i;
+		ans_elt = *to_p;
+		if (nodup0 && is_used->elts[ans_elt - 1])
+			continue;
+		if (*ans_p != NA_INTEGER
+		 && (select_mode == FIRST_HIT) != (ans_elt < *ans_p))
+			continue;
+		*ans_p = ans_elt;
 	}
 	UNPROTECT(1);
 	return ans;

src/R_init_S4Vectors.c
@@ -81,7 +81,7 @@ static const R_CallMethodDef callMethods[] = {
 
 /* Hits_class.c */
 	CALLMETHOD_DEF(Hits_new, 6),
-	CALLMETHOD_DEF(select_hits, 4),
+	CALLMETHOD_DEF(select_hits, 6),
 	CALLMETHOD_DEF(make_all_group_inner_hits, 2),
 
 /* Rle_class.c */

src/S4Vectors.h
@@ -901,7 +901,9 @@ SEXP select_hits(
 	SEXP from,
 	SEXP to,
 	SEXP nLnode,
-	SEXP select
+	SEXP nRnode,
+	SEXP select,
+	SEXP nodup
 );
 
 SEXP make_all_group_inner_hits(

commit 44f8a4d4cfc9ac156d2cee33170a923d5faeec8b

Fix regression in breakTies() introduced by commit 0fcc8684

DESCRIPTION
@@ -8,7 +8,7 @@ Description: The S4Vectors package defines the Vector and List virtual classes
 	interest (e.g. DataFrame, Rle, and Hits) are implemented in the
 	S4Vectors package itself (many more are implemented in the IRanges
 	package and in other Bioconductor infrastructure packages).
-Version: 0.19.9
+Version: 0.19.10
 Encoding: UTF-8
 Author: H. Pagès, M. Lawrence and P. Aboyoun
 Maintainer: Bioconductor Package Maintainer <maintainer@bioconductor.org>

R/Hits-class.R
@@ -504,7 +504,7 @@ breakTies <- function(x, method=c("first", "last"), rank) {
     if (!is(x, "Hits"))
         stop("'x' must be a Hits object")
     method <- match.arg(method)
-    to <- selectHits(x, method, rank)
+    to <- selectHits(x, method, rank=rank)
     .new_Hits("SortedByQueryHits", which(!is.na(to)), to[!is.na(to)],
               nLnode(x), nRnode(x), NULL)
 }