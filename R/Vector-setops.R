e3157433c47d384d634d3b73b7bd612f68d11b72
Michael Lawrence <michafla@gene.com>
2019-01-11 09:55:27 -0800

    need to call the original S4 generics

--- R/Vector-setops.R
+++ R/Vector-setops.R
@@ -8,19 +8,19 @@
 
 ### S3/S4 combo for union.Vector
 setMethod("union", c("Vector", "Vector"), function(x, y) unique(c(x, y)))
-union.Vector <- union
+union.Vector <- function(x, y, ...) union(x, y, ...)
 
 ### S3/S4 combo for intersect.Vector
 setMethod("intersect", c("Vector", "Vector"),
           function(x, y) unique(x[x %in% y]))
-intersect.Vector <- intersect
+intersect.Vector <- function(x, y, ...) intersect(x, y, ...)
 
 ### S3/S4 combo for setdiff.Vector
 setMethod("setdiff", c("Vector", "Vector"),
           function(x, y) unique(x[!(x %in% y)]))
-setdiff.Vector <- setdiff
+setdiff.Vector <- function(x, y, ...) setdiff(x, y, ...)
 
 ### S3/S4 combo for setequal.Vector
 setMethod("setequal", c("Vector", "Vector"),
           function(x, y) all(x %in% y) && all(y %in% x))
-setequal.Vector <- setequal
+setequal.Vector <- function(x, y, ...) setequal(x, y, ...)
