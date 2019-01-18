39bd2363b55a0f18ea65ef9011a823151d708e76
larman <michafla@gene.com>
2019-01-17 16:01:27 -0800

    return to calling callNextMethod() in union,Hits,Hits()

--- R/Hits-setops.R
+++ R/Hits-setops.R
@@ -9,10 +9,6 @@
 
 setMethod("union", c("Hits", "Hits"),
     function(x, y)
-        ## callNextMethod() is broken in R <= 3.4 if there is another "union"
-        ## generic in the cache (which is the case e.g. if the user loads the
-        ## lubridate package). So we avoid its use for now.
-        #as(callNextMethod(as(x, "Hits"), as(y, "Hits")), class(x))
-        as(union.Vector(as(x, "Hits"), as(y, "Hits")), class(x))
+        as(callNextMethod(as(x, "Hits"), as(y, "Hits")), class(x))
 )
 
