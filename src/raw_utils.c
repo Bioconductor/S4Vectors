68d8eb0c8c3376ff309b815ec8c9f7392ea0224f
Hervé Pagès <hpages@fredhutch.org>
2019-03-14 18:07:39 -0700

    Some optimization to the extract_character_from_raw_by_*() utilities

--- src/raw_utils.c
+++ src/raw_utils.c
@@ -1,20 +1,8 @@
 #include "S4Vectors.h"
+
 #include <stdlib.h>  /* for free() */
 
-
-static int memcpy_with_translation(char *dest, const char *src, int n,
-				   const int *lkup, int lkup_length)
-{
-	int i, c;
-
-	for (i = 0; i < n; i++) {
-		c = translate_byte(src[i], lkup, lkup_length);
-		if (c == NA_INTEGER)
-			break;
-		dest[i] = (char) c;
-	}
-	return i;
-}
+//#include <time.h>  /* for clock() */
 
 static void invalid_byte_error(char byte, int pos)
 {
@@ -22,59 +10,105 @@ static void invalid_byte_error(char byte, int pos)
 	      (int) byte, byte, pos);
 }
 
-/* Return a character vector of length 1 (single string). */
-static SEXP extract_bytes_by_positions_as_one_string(const char *x,
-		const int *pos, int npos,
-		SEXP lkup)
+static int memcpy_with_translation(char *dest, const char *src, int n,
+				   const int *lkup, int lkup_len)
 {
-	char *dest;
 	int i, c;
-	const char *src;
+
+	for (i = 0; i < n; i++) {
+		c = translate_byte(src[i], lkup, lkup_len);
+		if (c == NA_INTEGER)
+			break;
+		dest[i] = (char) c;
+	}
+	return i;
+}
+
+/* Return a single string (i.e. character vector of length 1). */
+static SEXP extract_bytes_by_positions_as_one_string(
+		const char *x, int x_len,
+		const int *pos, int npos,
+		const int *lkup, int lkup_len)
+{
+	char *dest, byte;
+	int i, pos_i, c;
 	SEXP ans, ans_elt;
 
+	//clock_t t0 = clock();
 	dest = (char *) malloc(npos);
 	if (dest == NULL)
 		error("memory allocation error in .Call entry point "
 		      "C_extract_character_from_raw_by_positions()");
+	//double dt = (1.0 * clock() - t0) / CLOCKS_PER_SEC;
+	//printf("time for malloc(): %e\n", dt);
+
+	x--;  /* so we can just do 'x[pos_i]' instead of 'x[pos_i - 1]' in
+	         the loop below */
+
+	//Surprisingly my timings show that the for loop below is faster
+	//when 'lkup' is not NULL (i.e. when bytes are translated) than
+	//when it's NULL (i.e. when bytes are NOT translated)!!! How could
+	//this possibly make any sense?!!
+	//t0 = clock();
 	for (i = 0; i < npos; i++) {
-		src = x + pos[i] - 1;
-		if (lkup == R_NilValue) {
-			dest[i] = *src;
+		pos_i = pos[i];
+		if (pos_i == NA_INTEGER || pos_i < 1 || pos_i > x_len) {
+			free(dest);
+			error("'pos[%d]' is NA or < 1 or > length(x)", i + 1);
+		}
+		byte = x[pos_i];
+		if (lkup == NULL) {
+			dest[i] = byte;
 		} else {
-			c = translate_byte(*src, INTEGER(lkup), LENGTH(lkup));
+			c = translate_byte(byte, lkup, lkup_len);
 			if (c == NA_INTEGER) {
 				free(dest);
-				invalid_byte_error(*src, pos[i]);
+				invalid_byte_error(byte, pos_i);
 			}
 			dest[i] = (char) c;
 		}
 	}
+	//dt = (1.0 * clock() - t0) / CLOCKS_PER_SEC;
+	//printf("time for for-loop: %e\n", dt);
+
+	//t0 = clock();
 	ans_elt = PROTECT(mkCharLen(dest, npos));
 	ans = PROTECT(ScalarString(ans_elt));
 	free(dest);
 	UNPROTECT(2);
+	//dt = (1.0 * clock() - t0) / CLOCKS_PER_SEC;
+	//printf("time for making SEXP: %e\n", dt);
 	return ans;
 }
 
-/* Return a character vector with one 1-letter string per position. */
-static SEXP extract_bytes_by_positions_as_strings(const char *x,
+/* Return a character vector **parallel** to the set of positions specified
+   via the 'pos' argument. Each element in the character vector is a 1-letter
+   string. */
+static SEXP extract_bytes_by_positions_as_strings(
+		const char *x, int x_len,
 		const int *pos, int npos,
-		SEXP lkup)
+		const int *lkup, int lkup_len)
 {
-	char dest[1];
-	int i, c;
-	const char *src;
+	char dest[1], byte;
+	int i, pos_i, c;
 	SEXP ans, ans_elt;
 
 	ans = PROTECT(NEW_CHARACTER(npos));
+	x--;  /* so we can just do 'x[pos_i]' instead of 'x[pos_i - 1]' in
+	         the loop below */
 	for (i = 0; i < npos; i++) {
-		src = x + pos[i] - 1;
-		if (lkup == R_NilValue) {
-			dest[0] = *src;
+		pos_i = pos[i];
+		if (pos_i == NA_INTEGER || pos_i < 1 || pos_i > x_len) {
+			UNPROTECT(1);
+			error("'pos[%d]' is NA or < 1 or > length(x)", i + 1);
+		}
+		byte = x[pos_i];
+		if (lkup == NULL) {
+			dest[0] = byte;
 		} else {
-			c = translate_byte(*src, INTEGER(lkup), LENGTH(lkup));
+			c = translate_byte(byte, lkup, lkup_len);
 			if (c == NA_INTEGER)
-				invalid_byte_error(*src, pos[i]);
+				invalid_byte_error(byte, pos_i);
 			dest[0] = (char) c;
 		}
 		ans_elt = PROTECT(mkCharLen(dest, 1));
@@ -85,10 +119,11 @@ static SEXP extract_bytes_by_positions_as_strings(const char *x,
 	return ans;
 }
 
-/* Return a character vector of length 1 (single string). */
+/* Return a single string (i.e. character vector of length 1). */
 static SEXP extract_bytes_by_ranges_as_one_string(const char *x,
 		const int *start, const int *width, int nranges,
-		int totalchars, SEXP lkup)
+		int totalchars,
+		const int *lkup, int lkup_len)
 {
 	char *dest;
 	int i, width_i, off;
@@ -103,13 +138,12 @@ static SEXP extract_bytes_by_ranges_as_one_string(const char *x,
 	for (i = 0; i < nranges; i++) {
 		src = x + start[i] - 1;
 		width_i = width[i];
-		if (lkup == R_NilValue) {
+		if (lkup == NULL) {
 			memcpy(dest + totalchars, src, width_i);
 		} else {
 			off = memcpy_with_translation(dest + totalchars, src,
 						      width_i,
-						      INTEGER(lkup),
-						      LENGTH(lkup));
+						      lkup, lkup_len);
 			if (off != width_i) {
 				free(dest);
 				invalid_byte_error(src[off], start[i] + off);
@@ -124,17 +158,19 @@ static SEXP extract_bytes_by_ranges_as_one_string(const char *x,
 	return ans;
 }
 
-/* Return a character vector with one string per range. */
+/* Return a character vector **parallel** to the set of ranges specified
+   via the 'start' and 'width' arguments. */
 static SEXP extract_bytes_by_ranges_as_strings(const char *x,
 		const int *start, const int *width, int nranges,
-		int maxwidth, SEXP lkup)
+		int maxwidth,
+		const int *lkup, int lkup_len)
 {
 	char *dest = NULL;
 	int i, width_i, off;
 	const char *src;
 	SEXP ans, ans_elt;
 
-	if (lkup != R_NilValue) {
+	if (lkup != NULL) {
 		dest = (char *) malloc(maxwidth);
 		if (dest == NULL)
 			error("memory allocation error in "
@@ -144,12 +180,11 @@ static SEXP extract_bytes_by_ranges_as_strings(const char *x,
 	for (i = 0; i < nranges; i++) {
 		src = x + start[i] - 1;
 		width_i = width[i];
-		if (lkup == R_NilValue) {
+		if (lkup == NULL) {
 			ans_elt = PROTECT(mkCharLen(src, width_i));
 		} else {
 			off = memcpy_with_translation(dest, src, width_i,
-						      INTEGER(lkup),
-						      LENGTH(lkup));
+						      lkup, lkup_len);
 			if (off != width_i) {
 				free(dest);
 				UNPROTECT(1);
@@ -167,44 +202,55 @@ static SEXP extract_bytes_by_ranges_as_strings(const char *x,
 }
 
 /*
- * Return a character vector with one 1-letter string per position if 'collapse'
- * is FALSE. Otherwise return a character vector of length 1 (single string).
+ * Use a single-pass algorithm.
+ * If 'collapse' is FALSE, return a character vector **parallel** to the set
+ * of positions specified via the 'pos' argument. Each element in the character
+ * vector is a 1-letter string.
+ * Otherwise return a single string (i.e. character vector of length 1).
  */
 SEXP _extract_bytes_by_positions(const char *x, int x_len,
 				 const int *pos, int npos,
 				 int collapse, SEXP lkup)
 {
-	int i, pos_i;
+	const int *lkup_p = NULL;
+	int lkup_len = 0;
 
-	if (!(lkup == R_NilValue || IS_INTEGER(lkup)))
-		error("'lkup' must an integer vector or NULL");
-
-	/* 1st pass: Check the positions. */
-	for (i = 0; i < npos; i++) {
-		pos_i = pos[i];
-		if (pos_i == NA_INTEGER || pos_i < 1 || pos_i > x_len)
-			error("'pos[%d]' is NA or < 1 or > length(x)", i + 1);
+	if (lkup != R_NilValue) {
+		if (!IS_INTEGER(lkup))
+			error("'lkup' must an integer vector or NULL");
+		lkup_p = INTEGER(lkup);
+		lkup_len = LENGTH(lkup);
 	}
 
-	/* 2nd pass: Extract the data into a character string. */
 	return collapse ?
-		extract_bytes_by_positions_as_one_string(x, pos, npos, lkup) :
-		extract_bytes_by_positions_as_strings(x, pos, npos, lkup);
+		extract_bytes_by_positions_as_one_string(x, x_len,
+							pos, npos,
+							lkup_p, lkup_len) :
+		extract_bytes_by_positions_as_strings(x, x_len,
+							pos, npos,
+							lkup_p, lkup_len);
 }
 
 /*
- * Return a character vector with one string per range if 'collapse' is FALSE.
- * Otherwise return a character vector of length 1 (single string).
+ * Use a 2-pass algorithm.
+ * If 'collapse' is FALSE, return a character vector **parallel** to the set
+ * of ranges specified via the 'start' and 'width' arguments.
+ * Otherwise return a single string (i.e. character vector of length 1).
  */
 SEXP _extract_bytes_by_ranges(const char *x, int x_len,
 			      const int *start, const int *width, int nranges,
 			      int collapse, SEXP lkup)
 {
-	int maxwidth, i, start_i, width_i, end_i;
+	const int *lkup_p = NULL;
+	int lkup_len = 0, maxwidth, i, start_i, width_i, end_i;
 	unsigned int totalchars;
 
-	if (!(lkup == R_NilValue || IS_INTEGER(lkup)))
-		error("'lkup' must an integer vector or NULL");
+	if (lkup != R_NilValue) {
+		if (!IS_INTEGER(lkup))
+			error("'lkup' must an integer vector or NULL");
+		lkup_p = INTEGER(lkup);
+		lkup_len = LENGTH(lkup);
+	}
 
 	/* 1st pass: Check the ranges and compute the total number of
 	   characters to extract or the width of the biggest range. */
@@ -237,10 +283,12 @@ SEXP _extract_bytes_by_ranges(const char *x, int x_len,
 
 	/* 2nd pass: Extract the data into a character string. */
 	return collapse ?
-		extract_bytes_by_ranges_as_one_string(x, start, width,
-					nranges, (int) totalchars, lkup) :
-		extract_bytes_by_ranges_as_strings(x, start, width,
-					nranges, maxwidth, lkup);
+		extract_bytes_by_ranges_as_one_string(x,
+				start, width, nranges, (int) totalchars,
+				lkup_p, lkup_len) :
+		extract_bytes_by_ranges_as_strings(x,
+				start, width, nranges, maxwidth,
+				lkup_p, lkup_len);
 }
 
 /* --- .Call ENTRY POINT --- */
