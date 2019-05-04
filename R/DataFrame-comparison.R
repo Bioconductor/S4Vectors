399dd3d41dc842bdca68df5cef4c11f3b4081eb8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-05-04 09:11:02 -0700

    Fixes to DF-specific methods.

--- R/DataFrame-comparison.R
+++ R/DataFrame-comparison.R
@@ -5,8 +5,13 @@ setMethod("sameAsLastROW", "DataFrame", function(x) {
     Reduce("&", is.diff)
 })
 
+# Necessary to avoid using match,List,List-method.
+setMethod("match", c("DataFrame", "DataFrame"), function (x, table, nomatch = NA_integer_, incomparables = NULL, ...) {
+    FUN <- selectMethod("match", c("Vector", "Vector"))
+    FUN(x, table, nomatch=nomatch, incomparables=incomparables)
+})
+
 setMethod("order", "DataFrame", function(..., na.last = TRUE, decreasing = FALSE, method = c("auto", "shell", "radix")) {
-    contents <- lapply(list(...), as.list)
-    contents <- unlist(contents, recursive=FALSE)
+    contents <- as.list(cbind(...))
     do.call(order, c(contents, list(na.last=na.last, decreasing=decreasing, method=method)))
 })
