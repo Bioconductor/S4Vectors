5f86d55a159aa86f48c6bac57f46726aad04e75f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-09-15 17:00:46 -0700

    Minor fixes to get things to work.

--- R/DataFrameFactor-class.R
+++ R/DataFrameFactor-class.R
@@ -1,9 +1,14 @@
 setClass("DataFrameFactor", contains="Factor", slots=c(levels="DataFrame"))
 
 DataFrameFactor <- function (x, levels, index = NULL, ...) {
-    f <- Factor(x, levels, index=index, ...)
-    f@levels <- DataFrame(as.list(f@levels)) # hack
-    as(f, "DataFrameFactor")
+    if (is.null(index)) {
+        levels <- sort(unique(x))
+        index <- match(x, levels) 
+        names(index) <- rownames(x)
+    }
+    out <- Factor(levels=seq_len(nrow(levels)), index=index, ...)
+    out@levels <- levels
+    as(out, "DataFrameFactor")
 }
 
 setMethod("dim", "DataFrameFactor", function(x) c(length(x), ncol(levels(x))))
@@ -15,19 +20,16 @@ setMethod("[[", "DataFrameFactor", function(x, i, j, ...) levels(x)[as.integer(x
 setMethod("$", "DataFrameFactor", function(x, name) x[[name]])
 
 setMethod("[", "DataFrameFactor", function(x, i, j, ..., drop=TRUE) {
-    if (!missing(j)) {
-        sub.levels <- levels(x)[,j,drop=FALSE]
-        new.levels <- unique(sub.levels)
-        x@index <- match(sub.levels, new.levels)[x@index]
-        x@levels <- new.levels
-    }
-
     if (!missing(i)) {
-        x <- extractROWS(x, i)
+        x <- callNextMethod()
+    } 
+    
+    if (!missing(j)) {
+        x@levels <- levels(x)[,j,drop=FALSE]
     }
 
-    if (drop && ncol(x)==1L) {
-        x <- x[[1]]
+    if (drop && ncol(levels(x))==1L) {
+        x <- levels(x)[as.integer(x),1]
     }
 
     x
